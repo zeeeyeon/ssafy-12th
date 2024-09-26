6321b6e400e4e00e65f0176e9738a25eaf00ca61
zeeeyeon <zeeyeonleee@gmail.com>
2024-09-26 10:36:45 +0900

    upload_to argument

--- WEB/django/07-staticfiles/articles/models.py
+++ WEB/django/07-staticfiles/articles/models.py
@@ -8,4 +8,5 @@ class Article(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     # pip install Pillow 설치 후 ImageField 사용 가능
     # upload_to : 미디어 루트 이후 images 공간을 만들겠음
-    image = models.ImageField(blank=True, upload_to='images/')
+    # image = models.ImageField(blank=True, upload_to='images/')
+    image = models.ImageField(blank=True, upload_to='%Y/%m/%d/')
