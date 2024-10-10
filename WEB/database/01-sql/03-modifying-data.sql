6bd42ad40dee0000d00224e7d3031d3348dc2d9b
zeeeyeon <zeeyeonleee@gmail.com>
2024-10-10 16:09:09 +0900

    sql

--- WEB/database/01-sql/03-modifying-data.sql
+++ WEB/database/01-sql/03-modifying-data.sql
@@ -21,3 +21,11 @@ set title = '제목 수정'
 where id = 1;
 
 -- 3. Delete data from table
+-- 표현식 : 평가를 통해 하나의 값을 반환하는 식.
+delete from articles
+where id in (
+    select id
+    from articles
+    order by created_at
+    limit 2
+    );
--- WEB/database/01-sql/04-multi-table-queries.sql
+++ WEB/database/01-sql/04-multi-table-queries.sql
@@ -37,7 +37,23 @@ VALUES
   ('제목4', '내용4', 4),
   ('제목5', '내용5', 1);
 
-
 -- INNER JOIN
+SELECT *
+FROM articles
+inner join users
+on articles.userId = users.id
+where users.id = 1;
 
--- LEFT JOIN
+-- LEFT JOIN(from절이 왼쪽 테이블), 게시글은 전부 다 불러옴 join 한 테이블은 null값으로 채워버림
+SELECT *
+FROM users
+left join articles
+on articles.userId = users.id
+where articles.id is null;
+
+-- RIGHT JOIN()
+SELECT *
+FROM articles
+inner join users
+on articles.userId = users.id
+where users.id = 1;
