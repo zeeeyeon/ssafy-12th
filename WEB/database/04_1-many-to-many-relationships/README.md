5cd56ef93eed453918472fd2e3eb6370fda72e5b
zeeeyeon <zeeyeonleee@gmail.com>
2024-10-15 11:26:37 +0900

    model relationships

---
+++ WEB/database/04_1-many-to-many-relationships/README.md
@@ -0,0 +1,75 @@
+# Many-to-Many relationships 04_1 🚀
+
+
+> ### 모델 관계 설정
+> - 팔로우 기능 구현
+> ![img.png](images/img.png)
+> ![img_1.png](images/img_1.png)
+> ![img_2.png](images/img_2.png)
+> ![img_3.png](images/img_3.png)
+
+> ### Fixtures
+> #### 초기 데이터 제공을 목적으로 함
+> ![img_4.png](images/img_4.png)
+> ![img_5.png](images/img_5.png)
+> ![img_6.png](images/img_6.png)
+
+> ### dumpdata
+> #### 데이터베이스의 모든 데이터를 추출
+> ![img_7.png](images/img_7.png)
+> ![img_8.png](images/img_8.png)
+> ![img_9.png](images/img_9.png)
+> ![img_10.png](images/img_10.png)
+> - 크게 권장하지 않음 !
+> ![img_46.png](images/img_46.png)
+
+> ### loaddata
+> #### Fixtures 데이터를 데이터베이스로 불러오기
+> ![img_11.png](images/img_11.png)
+> ![img_12.png](images/img_12.png)
+> ![img_13.png](images/img_13.png)
+> ![img_14.png](images/img_14.png)
+> ![img_45.png](images/img_45.png)
+
+> ### Improve query
+> #### query 개선하기 - 같은 결과를 얻기 위해 DB 측에 보내는 query 개수를 점차 줄여 조회하기
+> ![img_15.png](images/img_15.png)
+> ![img_16.png](images/img_16.png)
+
+> ### Annotate
+> ![img_17.png](images/img_17.png)
+> ![img_18.png](images/img_18.png)
+> ![img_19.png](images/img_19.png)
+> ![img_20.png](images/img_20.png)
+> ![img_21.png](images/img_21.png)
+> ![img_22.png](images/img_22.png)
+
+> ### select_related
+> ![img_23.png](images/img_23.png)
+> ![img_24.png](images/img_24.png)
+> ![img_25.png](images/img_25.png)
+> ![img_26.png](images/img_26.png)
+> ![img_27.png](images/img_27.png)
+> ![img_28.png](images/img_28.png)
+
+> ### prefetch_related
+> ![img_29.png](images/img_29.png)
+> ![img_30.png](images/img_30.png)
+> ![img_31.png](images/img_31.png)
+> ![img_32.png](images/img_32.png)
+> ![img_33.png](images/img_33.png)
+> ![img_34.png](images/img_34.png)
+
+> ### select_related & prefetch_related
+> ![img_35.png](images/img_35.png)
+> ![img_36.png](images/img_36.png)
+> ![img_37.png](images/img_37.png)
+> ![img_38.png](images/img_38.png)
+> ![img_39.png](images/img_39.png)
+> ![img_40.png](images/img_40.png)
+
+> ### exist method
+> ![img_41.png](images/img_41.png)
+> ![img_42.png](images/img_42.png)
+> ![img_43.png](images/img_43.png)
+> ![img_44.png](images/img_44.png)
---
+++ WEB/database/04_1-many-to-many-relationships/accounts/views.py
@@ -0,0 +1,112 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth import get_user_model
+
+# Create your views here.
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+@login_required
+def delete(request):
+    request.user.delete()
+    return redirect('articles:index')
+
+
+@login_required
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+@login_required
+def change_password(request, user_pk):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('articles:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/change_password.html', context)
+
+
+def profile(request, username):
+    # 어떤 유저의 프로필을 보여줄 것인지 유저를 조회(username을 사용하여)
+    person = get_user_model().objects.get(username=username)
+    context = {
+        'person': person
+    }
+    return render(request, 'accounts/profile.html', context)
+
+@login_required
+def follow(request, user_pk):
+    # 팔로우 요청을 보내는 대상
+    you = get_user_model().objects.get(pk=user_pk).get(pk=user_pk)
+    # 나 (팔로우 요청하는 사람)
+    me = request.user
+    # if me in you.followers.all:
+    #     you.followers.remove(me)
+    # else:
+    #     you.followers.add(me)
+
+    if you != me:
+        if me in you.followers.all():
+            you.followers.remove(me)
+        else:
+            you.followers.add(me)
+    return redirect('accounts:profile', you.username)
