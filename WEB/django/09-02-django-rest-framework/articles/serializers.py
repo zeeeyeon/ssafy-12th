88b635883772203d42ba54fe8cd9c4b72bfa9ba6
zeeeyeon <zeeyeonleee@gmail.com>
2024-10-17 10:46:27 +0900

    django-rest-framework

---
+++ WEB/django/09-02-django-rest-framework/articles/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import Article, Comment
+
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = (
+            'id',
+            'title',
+            'content',
+        )
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class CommentDetailSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Comment
+            fields = ('id', 'content')
+
+    comment_set = CommentDetailSerializer(many=True, read_only=True)
+    comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class ArticleTitleSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Article
+            fields = ('title',)
+
+    article = ArticleTitleSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        # read_only_fields = ('article',)
